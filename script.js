dc997cd9525423769291c3920daa96cf158d7597
Pavel <pitty_89@mail.ru>
2016-05-16 21:51:29 +0300

    PreventDefault on click slide

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
 $(document).ready(function(){
     $('.carousel').carousel({	interval: 4000 });
+    $('#carousel-example-generic .carousel-inner').find('.item a').on('click', function(e){
+        e.preventDefault();
+    });
 })
